7d16e5493c0879087f5d14973377328c43ff79d8
Rakibul Islam <rakibul-islam45@users.noreply.github.com>
2025-07-26 23:04:39 +0600

    Initial commit

---
+++ src/test/java/com/app/leave_manager/LeaveManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.leave_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
