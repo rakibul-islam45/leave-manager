7362673d02562746f396c2df52b4f2444a566aa5
Rakibul Islam <rakibul-islam45@users.noreply.github.com>
2025-07-29 01:16:04 +0600

    Common response format added

---
+++ src/main/java/com/app/leave_manager/reponse/ApiResponse.java
@@ -0,0 +1,24 @@
+package com.app.leave_manager.reponse;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+    @Builder.Default
+    private LocalDateTime timestamp = LocalDateTime.now();
+
+}
