f9f56ca80c7360843661589f2ef4fe70c77e239e
Rakibul Islam <rakibul-islam45@users.noreply.github.com>
2025-10-05 02:08:19 +0600

    added repository

--- src/main/java/com/app/leave_manager/repository/LeaveRequestRepository.java
+++ src/main/java/com/app/leave_manager/repository/LeaveRequestRepository.java
@@ -1,10 +1,15 @@
 package com.app.leave_manager.repository;
 
 import com.app.leave_manager.model.entities.LeaveRequest;
+import com.app.leave_manager.model.enums.Status;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long> {
+
+    Page<LeaveRequest> findAllByStatus(Status status, Pageable pageable);
 
 }
