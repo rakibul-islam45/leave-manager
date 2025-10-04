cd0d7ad6339c881ce3b4ee41bc86b645d427c5ac
Rakibul Islam <rakibul-islam45@users.noreply.github.com>
2025-10-05 00:34:04 +0600

    Removed tenant related code

--- src/main/java/com/app/leave_manager/repository/TenantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.leave_manager.repository;
-
-import com.app.leave_manager.model.entities.Tenant;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TenantRepository extends JpaRepository<Tenant,Long> {
-}
